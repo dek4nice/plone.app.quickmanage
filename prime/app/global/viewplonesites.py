a80ef5478f9b16d2362629e410b53e7971b198c0
Dmitriy Ilyenkov <admin911@list.ru>
2014-12-02 17:05:46 +0500

    upd @@yoursites - mailhost detector

--- prime/app/global/viewplonesites.py
+++ prime/app/global/viewplonesites.py
@@ -12,11 +12,18 @@ class YourSitesView(BrowserView):
 
     def __call__(self):
         self.update()
+        self.checkout_mail_hosts()
         return self.render()
 
     def update(self):
         self.sites = [zopeobject for zopeobject in self.context.objectValues() if zopeobject.meta_type == 'Plone Site'] # objectIds()
         self.siteactions = [{'title':action[0] , 'url':action[1]} for action in self.actions]
+
+    def checkout_mail_hosts(self):
+        self.mail_hosts_detected = list()
+        for site in self.sites:
+            if 'MailHost' in site.objectIds():
+                self.mail_hosts_detected.append(site.id)
 
     def render(self):
         return self.index()
